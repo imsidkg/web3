ffcdc37d35062f47d88e56f49315bc5015aecd60
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-08-13 21:34:40 +0530

    project initialization

---
+++ todo-rust/target/debug/deps/todo_rust-7bc3f4582833d8df.d
@@ -0,0 +1,5 @@
+/home/imsidkg/projects/web3/todo-rust/target/debug/deps/todo_rust-7bc3f4582833d8df.d: src/main.rs
+
+/home/imsidkg/projects/web3/todo-rust/target/debug/deps/libtodo_rust-7bc3f4582833d8df.rmeta: src/main.rs
+
+src/main.rs:
---
+++ todo-rust/target/debug/deps/todo_rust-df9bd4f144aad69c.d
@@ -0,0 +1,5 @@
+/home/imsidkg/projects/web3/todo-rust/target/debug/deps/todo_rust-df9bd4f144aad69c.d: src/main.rs
+
+/home/imsidkg/projects/web3/todo-rust/target/debug/deps/libtodo_rust-df9bd4f144aad69c.rmeta: src/main.rs
+
+src/main.rs:
