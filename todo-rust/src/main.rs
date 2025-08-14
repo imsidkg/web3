480fbce3171c1e2b43d9643eaf10ee9b4de269a3
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-08-14 12:45:45 +0530

    todo list functions added

--- todo-rust/src/main.rs
+++ todo-rust/src/main.rs
@@ -1,3 +1,60 @@
+#[derive(Debug)]
+enum TaskStatus {
+    Pending,
+    InProgress,
+    Completed,
+}
+
+struct Task {
+    id: u32,
+    description: String,
+    status: TaskStatus,
+    due_date: Option<String>,
+}
+
+struct TodoList {
+    tasks: Vec<Task>,
+}
+
+impl TodoList {
+    fn new() -> Self {
+        return Self { tasks: Vec::new() };
+    }
+
+    fn add_task(&mut self, description: String, due_date: Option<String>) {
+        let id = self.tasks.len() as u32 + 1;
+        self.tasks.push(Task {
+            id,
+            description,
+            status: TaskStatus::Pending,
+            due_date,
+        });
+    }
+
+    fn list_tasks(&self) {
+        println!(
+            "
+--- To-Do List ---"
+        );
+        for task in &self.tasks {
+            print!(
+                "ID: {} - {} (Status: {:?})",
+                task.id, task.description, task.status
+            );
+            match &task.due_date {
+                Some(date) => {
+                    print!(" - Due: {}", date);
+                }
+                None => {}
+            }
+            println!();
+        }
+        println!("--------------------");
+    }
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
