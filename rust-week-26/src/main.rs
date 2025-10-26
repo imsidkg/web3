f52793f4bcad917802e71e7380f3bf7615db08e1
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-10-26 15:44:53 +0530

    with  generics on functons

--- rust-week-26/src/main.rs
+++ rust-week-26/src/main.rs
@@ -1,9 +1,7 @@
-fn main() {}
-
-fn sum_32(a: u32, b: u32) -> u32 {
-    return a+b
+fn main() {
+    println!("{}" , sum(1,2))
 }
 
-fn sum_f2(a: f32, b: f32) -> f32 {
-    return a+b
+fn sum<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
+    a + b
 }
